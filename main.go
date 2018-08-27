88e22a2c9ce525ffffb8b7c39a2ddc5b9b66dd39
shwinpiocess <springwind_lee@163.com>
2018-08-27 18:50:25 +0800

    Add config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/shwinpiocess/k8s-go-demo/handlers"
 )
@@ -10,8 +11,13 @@ import (
 func main() {
 	log.Print("Starting the service...")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
 	router := handlers.Router()
 
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
